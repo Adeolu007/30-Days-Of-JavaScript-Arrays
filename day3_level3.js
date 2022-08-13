e73d79c1d771dbe9e98cf7f59cfb0ac25ce8ef42
Adeolu Odunuyi <93635641+Adeolu007@users.noreply.github.com>
2022-08-13 13:33:11 -0700

    Update day3_level3.js

--- day3_level3.js
+++ day3_level3.js
@@ -1,6 +1,5 @@
 let newContent =
   "Love is the best thing in this world. Some found their love and some are still looking for their love.";
-let wordOccurence = "love";
+const numLove = (newContent.match(/love/gi))
 
-let solution = newContent.split(wordOccurence).length - 1;
-console.log(solution);
+console.log(numLove.length)
